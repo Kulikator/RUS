96f87fbc7f5929e795e1124ec23dfd3071a41255
Silas Rafe <sirafe@e3r6p6.21-school.ru>
2018-11-22 16:08:32 +0300

    PISREL

---
+++ Desktop/display/display_file.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   display_file.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sirafe <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/21 16:38:22 by sirafe            #+#    #+#             */
+/*   Updated: 2018/11/21 16:39:38 by sirafe           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <fcntl.h>
+
+void	display(char *filename)
+{
+	int file;
+	char character;
+
+	file = open(filename, O_RDONLY);
+	if (file < 0)
+		return ;
+	while (read(file, &character, 1))
+		write(1, &character, 1);
+	close(file);
+}
+
+int		main(int argc, char *argv[])
+{
+	if (argc == 1)
+		write(2, "File name missing.\n", 19);
+	else if (argc > 2)
+		write(2, "Too many arguments.\n", 20);
+	else
+		display(argv[1]);
+	return (0);
+}
